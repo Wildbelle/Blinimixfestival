83b1d834b3545e0077196dd79a8571bb9256e689
Thomas Dubernet <thoms.dubernet@gmail.com>
2021-05-31 16:56:07 +0200

    [architecture] add routes, components, dependances

---
+++ assets/react/components/Event.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Event extends Component {
+  render() {
+    return (
+      <div>
+        Event Page
+      </div>
+    );
+  }
+}
+
+export default Event;
---
+++ assets/react/routes/FoodPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FoodPage extends Component {
+  render() {
+    return (
+      <div>
+        Food Page
+      </div>
+    );
+  }
+}
+
+export default FoodPage;
