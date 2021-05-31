521f457a2823d63160ccd1cd3708c56542000ccd
Thomas Dubernet <thoms.dubernet@gmail.com>
2021-05-31 16:26:30 +0200

    [config] add webpack & google analytics

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import './styles/app.scss';
 
 import React from 'react'
@@ -8,3 +9,17 @@ ReactDOM.render(
     <App />,
   document.getElementById('root')
 );
+=======
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+>>>>>>> 31e5cb7 ([config] add webpack & google analytics)
