a9986b7fe03163d392343b7f6d8e598101f845b4
JohnVy <contact@spiritvy.com>
2021-06-11 14:09:59 +0200

    Illus Map

--- assets/js/components/Navigation.jsx
+++ assets/js/components/Navigation.jsx
@@ -7,8 +7,17 @@ import { faDiceD6 } from '@fortawesome/free-solid-svg-icons'
 import { faUtensils } from '@fortawesome/free-solid-svg-icons'
 
 import logo from '../../img/LogoBliniMix.png'
+import InteractiveMap from '../routes/InteractiveMap';
 
 class Navigation extends Component {
+  
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      showLinks: false,
+    }
+  }
   
   render() {
     const { pink, blue } = themes
@@ -16,16 +25,27 @@ class Navigation extends Component {
     
     return (
       <React.Fragment>
-        <div className="nav-1">
+        <div className={`nav-1 ${this.state.showLinks ? "show-nav" : "hide-nav"} `}>
           <NavLink exact to="/map"><img className="navbrand" src={logo} alt="logo"/></NavLink>
           {!this.props.location.pathname.includes('map')
             &&  <div className="menu border">
-                  <div className="burger-box">
+                  <div className="burger-box" onClick={() => setShowLinks(!showLinks)}>
                     <div className="burger-bar"></div>
                   </div>
                 </div>
           }
         </div>
+        <div className="canvas-navburger">
+          <div className="navburger">
+            <NavLink className=" btn" onClick={() => handleShowLinks()} exact to="/food" >Str'eat food</NavLink>
+            <NavLink className=" btn" onClick={() => handleShowLinks()} exact to="/food" >Str'eat food</NavLink>
+            <NavLink className=" btn" onClick={() => handleShowLinks()} exact to="/food" >Str'eat food</NavLink>
+            <NavLink className=" btn" onClick={() => handleShowLinks()} exact to="/food" >Str'eat food</NavLink>
+            <NavLink className=" btn" onClick={() => handleShowLinks()} exact to="/food" >Str'eat food</NavLink>
+          </div>
+        </div>
+      
+
         {navVisible &&
           <div className="nav-2">
             <NavLink 
