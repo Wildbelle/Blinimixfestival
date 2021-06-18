c2e2d4325f3d4d994e8ad2527d8565d9e8901faa
JohnVy <contact@spiritvy.com>
2021-06-18 11:03:45 +0200

    URI

--- assets/js/components/Theme.jsx
+++ assets/js/components/Theme.jsx
@@ -12,14 +12,6 @@ const params = {
   pm: 2377172,
   divider: encodeURIComponent('|'),
   pagename: encodeURIComponent('LP Blinimix Festival')
-    (function () {
-      var s = document.createElement('script');
-      s.type = 'text/javascript';
-      s.async = true;
-      s.src = 'https://s2.adform.net/banners/scripts/st/trackpoint-async.js';
-      var x = document.getElementsByTagName('script')[0];
-      x.parentNode.insertBefore(s, x);
-    })()
 };
 
 adform.initialize(params);
